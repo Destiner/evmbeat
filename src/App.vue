fff2e6f6db844bfbb5a5fcf9baea2f36c8a051af
Timur Badretdinov <destinerx@gmail.com>
2024-01-01 17:56:05 +0000

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <router-view />
+    <footer>Inspired by Mintscan. Not affiliated with L2Beat.</footer>
   </div>
 </template>
 
@@ -20,10 +21,23 @@ import '@fontsource/ibm-plex-mono';
     'Lucida Console', monospace;
 }
 
+#app {
+  display: flex;
+  flex-direction: column;
+}
+
 body {
   font-family: var(--font-mono);
   background: #0e0e0e;
   color: #9d9d9d;
+}
+
+footer {
+  min-height: 160px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 12px;
 }
 
 /* Reset */
@@ -63,9 +77,5 @@ h4,
 h5,
 h6 {
   overflow-wrap: break-word;
-}
-
-#app {
-  isolation: isolate;
 }
 </style>
